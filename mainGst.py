208f92b3ab2d762424883c11831bf563f7a129eb
sfrullo <sfrulloo@gmail.com>
2015-06-03 13:36:37 +0200

    Refactoring player's functionalities in Player.py module

--- mainGst.py
+++ mainGst.py
@@ -1,16 +1,9 @@
-import gi
-gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gst, GstVideo  # @UnresolvedImport
-#print(Gst.version())
-
-
 import tkinter as Tk
 from PIL import Image, ImageTk
 from os import path
 from pprint import pprint
 
-GObject.threads_init()
-Gst.init(None)
+from player import *
      
 uri1 = 'file://' + path.join(path.dirname(path.abspath(__file__)), 'v1.avi')
 uri2 = 'file://' + path.join(path.dirname(path.abspath(__file__)), 'v2.avi')
@@ -25,29 +18,18 @@ class App(Tk.Frame):
         self.createWidgets()
         self.init_player()
     
-    def on_play(self):
-        self.xid1 = self.videocontainer.winfo_id()
-        self.xid2 = self.video2container.winfo_id()
-        self.pipeline_vid1.set_state(Gst.State.PLAYING)
-        self.pipeline_vid2.set_state(Gst.State.PLAYING)
-    
-    
-    def on_stop(self):
-        self.pipeline_vid1.set_state(Gst.State.NULL)
-        self.pipeline_vid2.set_state(Gst.State.NULL)
-    
-    
+     
     def createWidgets(self):
         
-        self.videocontainer = Tk.Frame(self, bg='white')
-        self.videocontainer.configure(width=250, height=250)
-        self.videocontainer.anchor(Tk.CENTER)
-        self.videocontainer.grid(column = 0, row=0)
+        self.video = Tk.Frame(self, bg='white')
+        self.video.configure(width=250, height=250)
+        self.video.anchor(Tk.CENTER)
+        self.video.grid(column = 0, row=0)
         
-        self.video2container = Tk.Frame(self, bg='white')
-        self.video2container.configure(width=250, height=250)
-        self.video2container.anchor(Tk.CENTER)
-        self.video2container.grid(column = 1, row=0)
+        self.video2 = Tk.Frame(self, bg='white')
+        self.video2.configure(width=250, height=250)
+        self.video2.anchor(Tk.CENTER)
+        self.video2.grid(column = 1, row=0)
         
         self.buttoncontainer  = Tk.LabelFrame(relief='flat')
         self.buttoncontainer.grid(column = 0, row=1)
@@ -55,60 +37,43 @@ class App(Tk.Frame):
         self.playbutton.grid(column=0, row=0)
         self.stopbutton = Tk.Button(self.buttoncontainer, text='Stop', command=self.on_stop)
         self.stopbutton.grid(column = 1, row=0)
+        self.revbutton = Tk.Button(self.buttoncontainer, text='Rew', command=self.on_rew)
+        self.revbutton.grid(column=0, row=1)
+        self.ffwdbutton = Tk.Button(self.buttoncontainer, text='FFWD', command=self.on_ffwd)
+        self.ffwdbutton.grid(column = 1, row=1)
         
-    
-    def on_sync_message(self, bus, msg):
-        if msg.get_structure().get_name() == 'prepare-window-handle':
-            print('prepare-window-handle')
-            if bus.get_name() == 'vid1':
-                msg.src.set_window_handle(self.xid1)
-            else:
-                msg.src.set_window_handle(self.xid2)
-    
-    def on_error(self, bus, msg):
-        print('on_error():', msg.parse_error())
-    
-    
-    def on_eos(self, bus, msg):
-        print('on_eos(): seeking to start of video')
-        self.pipeline_vid1.seek_simple(
-            Gst.Format.TIME,        
-            Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,
-            0
-        )
-        self.pipeline_vid2.seek_simple(
-            Gst.Format.TIME,        
-            Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,
-            0
-        )
-    
-    
+            
     def init_player(self):
                 
-        self.pipeline_vid1 = Gst.Pipeline()
-        self.pipeline_vid2 = Gst.Pipeline()
+        self.player1 = Player(name='Player1')
+        self.player2 = Player(name ='Player2')
         
-        self.bus1 = self.pipeline_vid1.get_bus()
-        self.bus1.set_name('vid1')
-        self.bus2 = self.pipeline_vid2.get_bus()
-        self.bus2.set_name('vid2')
+        self.player1.set_xid(self.video.winfo_id())
+        self.player2.set_xid(self.video2.winfo_id())
+
+        self.player1.set_media(uri1)
+        self.player2.set_media(uri2)
         
-        for b in [self.bus1, self.bus2]: 
-            b.add_signal_watch()
-            b.connect('message::eos', self.on_eos)
-            b.connect('message::error', self.on_error)
-        
-            b.enable_sync_message_emission()
-            b.connect('sync-message::element', self.on_sync_message)
-        
-        self.playbin_vid1 = Gst.ElementFactory.make('playbin', None)
-        self.playbin_vid2 = Gst.ElementFactory.make('playbin', None)
-        self.pipeline_vid1.add(self.playbin_vid1)
-        self.pipeline_vid2.add(self.playbin_vid2)
-        
-        self.playbin_vid1.set_property('uri', uri1)
-        self.playbin_vid2.set_property('uri', uri2)
-        
+        self.players = [self.player1, self.player2]
+
+
+    def on_play(self):
+        for p in self.players:
+            p.play()
+    
+    def on_stop(self):
+        for p in self.players:
+            p.stop()
+    
+    def on_rew(self):
+        for p in self.players:
+            p.rew()
+    
+    def on_ffwd(self):
+        for p in self.players:
+            p.ffwd()
+
+
 
 if __name__ == '__main__':
     app = App()
---
+++ player.py
@@ -0,0 +1,91 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GstVideo  # @UnresolvedImport
+#print(Gst.version())
+
+
+GObject.threads_init()
+Gst.init(None)
+
+
+class Player:
+    
+
+    def __init__(self, xid=None, name='Player'):
+        self.__xid = xid
+        self.__media = None
+        self.name = name
+        
+        self.pipeline = Gst.Pipeline()
+        
+        self.bus = self.pipeline.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect('message::eos', self.on_eos)
+        self.bus.connect('message::error', self.on_error)
+        self.bus.enable_sync_message_emission()
+        self.bus.connect('sync-message::element', self.on_sync_message)
+
+    
+    def play(self):
+        self.pipeline.set_state(Gst.State.PLAYING)
+        
+    def stop(self):
+        self.pipeline.set_state(Gst.State.NULL)
+        
+    def rew(self):
+        rc, pos = self.pipeline.query_position(Gst.Format.TIME)
+        newpos = pos - (10 * 10**8)
+        self.pipeline.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, newpos)
+    
+    def ffwd(self):
+        rc, pos = self.pipeline.query_position(Gst.Format.TIME)
+        newpos = pos + (10 * 10**8)
+        self.pipeline.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, newpos)
+    
+    
+    def on_eos(self, bus, msg):
+        print(self.name, 'on_eos(): seeking to start of video')
+        self.pipeline_vid1.seek_simple(
+            Gst.Format.TIME,        
+            Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,
+            0
+        )
+    
+    
+    def on_error(self, bus, msg):
+        print(self.name, 'on_error():', msg.parse_error())
+    
+    
+    def on_sync_message(self, bus, msg):
+        if msg.get_structure().get_name() == 'prepare-window-handle':
+            print('prepare-window-handle')
+            msg.src.set_window_handle(self.__xid)
+    
+    
+    def get_xid(self):
+        return self.__xid
+
+    def set_xid(self, value):
+        print('Set new xid value:\nPrevious:{} -> New:{}'.format(self.__xid, value))
+        self.__xid = value
+
+    def del_xid(self):
+        del self.__xid
+
+    xid = property(get_xid, set_xid, del_xid)
+
+
+    def get_media(self):
+        return self.__media
+
+    def set_media(self, value):
+        print('Set new Media: {}'.format(value))
+        self.__media = value
+        self.playbin = Gst.ElementFactory.make('playbin', None)
+        self.playbin.set_property('uri', self.__media)
+        self.pipeline.add(self.playbin)
+
+    def del_media(self):
+        del self.__media
+
+    media = property(get_media, set_media, del_media)
